7714438b3d4276f9671e434b41bda0297ed38183
Mihail Gumennii <mihail.gumennii@gmail.com>
2026-05-19 20:42:16 +0200

    feat: swap home page

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,15 +1,5 @@
 import type { Metadata } from "next";
-import Header from "@/app/components/modules/home/Header";
-import Hero from "@/app/components/modules/home/Hero";
-import Results from "@/app/components/modules/home/Results";
-import { VideoTestimonials } from "@/app/components/modules/home/VideoTestimonials";
-import { HowItWorks } from "@/app/components/modules/home/HowItWorks";
-import { Reviews } from "@/app/components/modules/home/Reviews";
-import { Medications } from "@/app/components/modules/home/Medications";
-import { FAQ } from "@/app/components/modules/home/FAQ";
-import { FinalCTA } from "@/app/components/modules/home/FinalCTA";
-import { Footer } from "@/app/components/Footer";
-import { AnchorScrollFix } from "@/app/components/modules/home/AnchorScrollFix";
+import EmbeddablesScript from "../components/EmbeddablesScript";
 
 export const metadata: Metadata = {
   title: "InstaRx - Lose Weight With GLP-1 Meds - Get Started For Just $199",
@@ -26,19 +16,8 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
-      <AnchorScrollFix />
-      <Header />
-      <main>
-        <Hero />
-        <Results />
-        <VideoTestimonials />
-        <HowItWorks />
-        <Reviews />
-        <Medications />
-        <FAQ />
-        <FinalCTA />
-      </main>
-      <Footer />
+      <EmbeddablesScript />
+      <savvy id="flow_a22jeg0agf2ch469bbf95406"></savvy>
     </>
   );
 }
